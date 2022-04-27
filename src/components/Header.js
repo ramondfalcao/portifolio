34d2f03c6ae7b23125304e1f0ef0c23433bdccd8
Ramond Falcão <ramondfalcao@gmail.com>
2022-04-27 13:07:04 -0300

    Estruturação de páginas

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>Work</li>
+        <li>About me</li>
+        <li>Get in touch</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/GetInTouch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GetInTouch() {
+  return (
+    <h1>Get in touch</h1>
+  );
+}
+
+export default GetInTouch;
