e4e607574397d3177180f3c44b70572d4d18c32f
Ramond Falcão <ramondfalcao@gmail.com>
2022-05-05 13:26:25 -0300

    Desktop Version - Page  Home - em andamento

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import './Home.css'
 import './button.css'
 import About from '../components/About';
 import Footer from '../components/Footer';
+import imgHome from '../assets/imgHome.svg';
 
 function Home(props) {
   const {history} = props
@@ -13,18 +14,22 @@ function Home(props) {
       <Header props={props} />
       <main className="main-home">
         <section className="section1-home">
-          <h1 id="title">Hi, I'm Ramond Falcão, a Front End developer.</h1>
-          <p id="paragraph">I am passionate about developing Front End applications, while working collaboratively and solving user problems.</p>
-          <button
-            onClick={() => history.push('/works')}
-            type="button"
-            id="button-work-main"
-          >
-            <span class="text">Works</span>
-          </button>
+          <section className='div-home'>
+            <h1 id="title">Hi, I'm Ramond Falcão, a Front End developer.</h1>
+            <p id="paragraph">I am passionate about developing Front End applications, while working collaboratively and solving user problems.</p>
+            <button
+              onClick={() => history.push('/works')}
+              type="button"
+              id="button-work-main"
+            >
+              <span class="text">Works</span>
+            </button>
+          </section>
+          <div className='section-img-home'>
+            <img className="img-home" src={imgHome} alt="" />
+          </div>
         </section>
         <About />
-        {/* <Skills /> */}
         <Footer />
       </main>
     </>
