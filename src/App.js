e879bf8789903035da8960df9061bf6dfd6f7ee3
Ramond Falcão <ramondfalcao@gmail.com>
2022-05-12 10:57:40 -0300

    Implementando Context Api

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Works from './pages/Works';
 import GetInTouch from './pages/GetInTouch';
+import PortifolioProvider from './context/PortifolioProvider';
+
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route path="/works" component={ Works } />
-      <Route path="/getintouch" component={ GetInTouch } />
-    </Switch>
+    <PortifolioProvider>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/works" component={ Works } />
+        <Route path="/getintouch" component={ GetInTouch } />
+      </Switch>
+    </PortifolioProvider>
   );
 }
 
---
+++ src/context/PortifolioProvider.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import portfolioContext from './PortfolioContext';
+
+function PortifolioProvider({ children }) {
+  const [toggleDark, setToggleDark] = useState(false);
+
+  const contextValue = {
+    toggleDark,
+  };
+
+  return (
+    <portfolioContext.Provider value={ contextValue }>
+      {children}
+    </portfolioContext.Provider>
+  );
+}
+
+PortifolioProvider.propTypes = {
+  children: PropTypes.objectOf(PropTypes.node).isRequired,
+};
+
+export default PortifolioProvider;
